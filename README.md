e2ec8f005d94640968938671668fc8ca42ccd666
Thang Nguyen <mrwin95@gmail.com>
2024-03-04 23:25:53 -0700

    completed chats topbar

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### This is the sample template of react app with typescript
+
+## free photos https://www.pexels.com/new-photos/
--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import {HiOutlineVideoCamera, HiUserPlus, HiOutlineSquaresPlus} from 'react-icons/hi2'
 
 export default function Chat() {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+        <div className="chatInfo">
+            <span>Win</span>
+            <div className="chatIcons">
+            <HiOutlineVideoCamera />
+            <HiUserPlus />
+            <HiOutlineSquaresPlus />
+            </div>
+        </div>
+    </div>
   )
 }
