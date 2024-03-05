0ee54475b90a34671b7eb7f1a9ec24e3c324ebe0
Thang Nguyen <mrwin95@gmail.com>
2024-03-04 17:51:31 -0700

    completed sidebar

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function Chats() {
   return (
-    <div>Chats</div>
+    <div className='chats'>
+         <div className="userChat">
+            <img src="https://images.pexels.com/photos/20466748/pexels-photo-20466748/free-photo-of-a-woman-in-a-red-dress-standing-on-a-street.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt="" />
+            <div className="userChatInfo">
+                <span>Win</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="userChat">
+            <img src="https://images.pexels.com/photos/20466748/pexels-photo-20466748/free-photo-of-a-woman-in-a-red-dress-standing-on-a-street.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt="" />
+            <div className="userChatInfo">
+                <span>Win</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="userChat">
+            <img src="https://images.pexels.com/photos/20466748/pexels-photo-20466748/free-photo-of-a-woman-in-a-red-dress-standing-on-a-street.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt="" />
+            <div className="userChatInfo">
+                <span>Win</span>
+                <p>Hello</p>
+            </div>
+        </div>
+    </div>
   )
 }
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -6,6 +6,12 @@ export default function Search() {
         <div className="searchForm">
             <input type='text' placeholder='find a user '/>
         </div>
+        <div className="userChat">
+            <img src="https://images.pexels.com/photos/20466748/pexels-photo-20466748/free-photo-of-a-woman-in-a-red-dress-standing-on-a-street.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt="" />
+            <div className="userChatInfo">
+                <span>Win</span>
+            </div>
+        </div>
     </div>
   )
 }
